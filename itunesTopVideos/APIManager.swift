d82311af5ce371d9c05dfd859b8f4f99b95358d6
Prateek-Mahawar <prateekmahawar@gmail.com>
2016-07-12 20:56:04 +0530

    API MANAGER WITHOUT CLASS

--- itunesTopVideos/APIManager.swift
+++ itunesTopVideos/APIManager.swift
@@ -20,16 +20,30 @@ class APIManager {
         
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
-            
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession Successful")
-                    print(data)
                 }
-            }
+            } else {
+                print(data)
+                do {
+    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority,0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSERILIZATION succesful")
+                            }
+                        }
+                    }
+            } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "error in serialization")
+                    }
+                }
         }
+}
         task.resume()
     }
 }
